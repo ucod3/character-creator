35da97b2c69d00cef971e0dc88f611ac58b2a4c2
Usman Butt <usman.butt@outlook.com>
2023-12-27 21:23:00 -0500

    Refactor rendering logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App tab='home' />);
+const root = document.getElementById('root');
+if (root) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
